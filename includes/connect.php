3bae82e6fdb7bf01054e7bc3198c7c791dae4fb2
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-20 10:45:22 +0800

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -14,7 +14,7 @@ if($production){
     $db_database	= 'oroars';
 }
 
-    $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   // $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
+  //  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
