edc8351e3a5b6b054845c3599fee9f206fca8930
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-20 14:19:50 +0800

    Update auth.php

--- admin/includes/auth.php
+++ admin/includes/auth.php
@@ -1,5 +1,6 @@
-<?php session_start();
+<?php 
 	include_once 'connect.php';
+	session_start();
 
 	//Check whether the session variable SESS_MEMBER_ID is present or not
 	if(!isset($_SESSION['username']) || (trim($_SESSION['username']) == '')) {
