0ee0364a82d0892996f563f8669c6010eb4660c8
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-20 10:48:40 +0800

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -14,7 +14,16 @@ if($production){
     $db_database	= 'oroars';
 }
 
-   // $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
-  //  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //$db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
+    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try {
+        $db = new PDO("mysql:host=$db_host;dbname=myDB", $db_user, $db_pass);
+        // set the PDO error mode to exception
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+      } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+      }
 
 ?>
