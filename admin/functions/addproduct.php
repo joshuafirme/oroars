838961c37f25bfe1892f2e14712a92325096e23f
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-20 19:37:38 +0800

    Update addproduct.php

--- admin/functions/addproduct.php
+++ admin/functions/addproduct.php
@@ -1,6 +1,6 @@
 <?php
-include_once 'includes/auth.php';
-include_once 'includes/connect.php';
+include_once '../includes/auth.php';
+include_once '../includes/connect.php';
 $ProductName = $_POST['ProductName'];
 $CategoryID = $_POST['CategoryID'];
 $SubCategoryID = $_POST['SubCategoryID'];
