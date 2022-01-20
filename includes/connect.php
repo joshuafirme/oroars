c348c498c17178fe232b29c522e8bd5a492d6247
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-20 11:35:06 +0800

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -18,7 +18,7 @@ if($production){
     //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     try {
-        $db = new PDO("mysql:host=$db_host;dbname=myDB", $db_user, $db_pass);
+        $db = new PDO("mysql:host=$db_host;dbname=$db_database", $db_user, $db_pass);
         // set the PDO error mode to exception
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo "Connected successfully";
