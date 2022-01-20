30b4448fea689b39bab0f2c3277dfb3f01f6d757
josh <61042416+joshuafirme@users.noreply.github.com>
2022-01-20 15:56:52 +0800

    debug

--- includes/auth.php
+++ includes/auth.php
@@ -1,8 +1,5 @@
-<?php 
-	include_once 'connect.php';
-
-
-	//Check whether the session variable SESS_MEMBER_ID is present or not
+<?php include_once 'connect.php';
+//Check whether the session variable SESS_MEMBER_ID is present or not
 	if(!isset($_SESSION['username']) || (trim($_SESSION['username']) == '')) {
 		header("location: index.php");
 		exit();
